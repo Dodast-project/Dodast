622169dc2c47b550ed0fab8b93417da5afd23453
HeliaNasirloo6 <helianasirloo@gmail.com>
2026-06-29 20:04:59 +0330

    init commit

---
+++ src/main/java/com/example/dodast/DodastApplication.java
@@ -0,0 +1,13 @@
+package com.example.dodast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DodastApplication.class, args);
+    }
+
+}
