1382c6ec2ac3ddfbe41d9bdf25ba133e0a93a226
HeliaNasirloo6 <helianasirloo@gmail.com>
2026-06-30 02:14:32 +0330

    Initial multi-module structure

---
+++ frontend/src/test/java/com/example/dodast/DodastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dodast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
