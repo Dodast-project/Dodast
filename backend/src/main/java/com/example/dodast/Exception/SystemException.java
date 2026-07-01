38dabedef05145619620c4303a8946d0c47081d5
Raman <raman.rezaei2007@gmail.com>
2026-07-01 10:15:07 +0330

    new : initialized classes for designing register and login for user 	Controller package -> API requests 	Service package -> program logic 	Repository package -> connection with database 	Model package -> all object used in program 	Model/Enums package -> all enums used in program 	DTO package -> datas that exchanged between the API and program

---
+++ backend/src/main/java/com/example/dodast/Exception/SystemException.java
@@ -0,0 +1,5 @@
+package com.example.dodast.Exception;
+
+public class SystemException extends Exception{
+
+}   
---
+++ backend/src/main/java/com/example/dodast/controller/AuthController.java
@@ -0,0 +1,5 @@
+package com.example.dodast.Controller;
+
+public class AuthController {
+    
+}
