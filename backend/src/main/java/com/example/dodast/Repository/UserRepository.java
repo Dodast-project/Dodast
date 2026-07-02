829004d7821e8fe0bc0b6f6d46aede6d5d293d2d
Raman <raman.rezaei2007@gmail.com>
2026-07-02 11:03:28 +0330

    new : user repository design with requirements 	can be used for check existency of user by email ot username (identifier). 	can be used to find user by its username and email (identifier). 	it is a way to connect to database.

--- backend/src/main/java/com/example/dodast/Repository/UserRepository.java
+++ backend/src/main/java/com/example/dodast/Repository/UserRepository.java
@@ -1,5 +1,15 @@
 package com.example.dodast.Repository;
 
-public class UserRepository {
-    
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.dodast.Model.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsernameOrEmail(String username, String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
