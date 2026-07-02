f0eef5a08a2a2f85d89190e1714492bdb02b9777
Raman <raman.rezaei2007@gmail.com>
2026-07-02 11:20:23 +0330

    new : add login request to store requests from frontend. 	it can be easily used for implementing logic in AuthService. 	spring boot maps json file recived form frontend and store values in 	login requset object.

--- backend/src/main/java/com/example/dodast/DTO/Auth/LoginRequest.java
+++ backend/src/main/java/com/example/dodast/DTO/Auth/LoginRequest.java
@@ -1,5 +1,25 @@
 package com.example.dodast.DTO.Auth;
 
 public class LoginRequest {
-    
+    private String identifier;
+    private String password;
+
+    public LoginRequest(){}
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
