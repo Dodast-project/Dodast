738eb6d3841bc21e22a19bc5fb3aba2ef7b76aea
Raman <raman.rezaei2007@gmail.com>
2026-07-07 18:02:12 +0330

    update : add new exception handler for validation
    	it helps to have better response in front for all requirements

--- backend/src/main/java/com/example/dodast/Exception/GlobalExceptionHandling.java
+++ backend/src/main/java/com/example/dodast/Exception/GlobalExceptionHandling.java
@@ -1,17 +1,40 @@
 package com.example.dodast.Exception;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandling{
 
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<ApiErrorResponse> handleApiException(ApiException e){
         ApiErrorResponse response = new ApiErrorResponse(e.getMessage(), e.getStatus().value());
         return ResponseEntity.status(e.getStatus()).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrorResponse> handleValidationException(MethodArgumentNotValidException e){
+        HashMap<String, String> errors = new HashMap<>();
+
+        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+
+        for(FieldError error: fieldErrors){
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+
+            errors.put(fieldName, errorMessage);
+        }
+
+        ValidationErrorResponse response = new ValidationErrorResponse("Validation error", HttpStatus.BAD_REQUEST.value(), errors);
+
+        return ResponseEntity.badRequest().body(response);
     }
 
     @ExceptionHandler(Exception.class)
