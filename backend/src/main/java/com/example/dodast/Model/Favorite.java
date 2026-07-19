37eace231f96a1fcab45f95880143b63c0c6399b
Raman <raman.rezaei2007@gmail.com>
2026-07-20 00:28:17 +0330

    new : add new entity (favorite)
    	it keeps user and advertisement.

---
+++ backend/src/main/java/com/example/dodast/Model/Favorite.java
@@ -0,0 +1,38 @@
+package com.example.dodast.Model;
+
+import org.hibernate.annotations.ManyToAny;
+import com.example.dodast.Model.User;
+import com.example.dodast.Model.Advertisement;
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Favorite{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY;
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "advertisement_id")
+    private Advertisement advertisement;
+}
