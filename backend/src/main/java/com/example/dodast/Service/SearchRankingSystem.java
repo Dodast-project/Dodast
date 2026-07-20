18868e1e0c4934b0a155e70c588fb098d611f369
Raman <raman.rezaei2007@gmail.com>
2026-07-20 12:12:27 +0330

    fix : fixed SearchRankingSystem and add @Component annotation

--- backend/src/main/java/com/example/dodast/Service/SearchRankingSystem.java
+++ backend/src/main/java/com/example/dodast/Service/SearchRankingSystem.java
@@ -1,17 +1,21 @@
 package com.example.dodast.Service;
 
 import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
 
 import com.example.dodast.Model.Enums.SearchSortBy;
 
+@Component
 public class SearchRankingSystem {
+
     public Sort createSort(SearchSortBy sortBy){
         switch (sortBy) {
             case LOWESTPRICE:
                 return Sort.by(Sort.Direction.ASC, "price");
-        
+
             case HIGHESTPRICE:
                 return Sort.by(Sort.Direction.DESC, "price");
+
             default:
                 return Sort.unsorted();
         }
