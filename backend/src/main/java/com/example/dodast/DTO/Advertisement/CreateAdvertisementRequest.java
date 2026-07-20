900bdc75882ff59092196046b807a757391b7298
Raman <raman.rezaei2007@gmail.com>
2026-07-20 15:25:04 +0330

    new : add list of images as multiparts in CreateAdvertisement to store incoming input

--- backend/src/main/java/com/example/dodast/DTO/Advertisement/CreateAdvertisementRequest.java
+++ backend/src/main/java/com/example/dodast/DTO/Advertisement/CreateAdvertisementRequest.java
@@ -4,6 +4,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import org.springframework.web.multipart.MultipartFile;
+import java.util.List;
 
 public class CreateAdvertisementRequest {
 
@@ -27,6 +29,8 @@ public class CreateAdvertisementRequest {
 
     @NotNull
     private Long cityId;
+
+    private List<MultipartFile> images;
 
     public CreateAdvertisementRequest() {
     }
@@ -77,6 +81,14 @@ public class CreateAdvertisementRequest {
 
     public void setCityId(Long cityId) {
         this.cityId = cityId;
+    }
+
+    public List<MultipartFile> getImages() {
+        return images;
+    }
+
+    public void setImages(List<MultipartFile> images) {
+        this.images = images;
     }
 
 }
