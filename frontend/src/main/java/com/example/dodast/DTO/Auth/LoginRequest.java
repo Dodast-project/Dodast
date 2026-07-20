ef9fa11a58c02686b55a23f4e72f1658752b7a4a
Raman <raman.rezaei2007@gmail.com>
2026-07-21 01:03:09 +0330

    new : add login request dto to store infos given in login and send it to backend

---
+++ frontend/src/main/java/com/example/dodast/DTO/Auth/LoginRequest.java
@@ -0,0 +1,23 @@
+package com.example.dodast.DTO.Auth;
+
+
+public class LoginRequest {
+
+    private String identifier;
+
+    private String password;
+
+    public LoginRequest(String identifier, String password){
+        this.identifier = identifier;
+        this.password = password;
+    }
+
+    public String getIdentifier(){
+        return identifier;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+}
