8d98945d2108b8584c8d6dd5e2495e279befca41
Raman <raman.rezaei2007@gmail.com>
2026-07-21 19:17:56 +0330

    new : add image response dto to store images url and get required section

---
+++ frontend/src/main/java/com/example/dodast/DTO/Advertisement/ImageResponse.java
@@ -0,0 +1,19 @@
+package com.example.dodast.DTO.Advertisement;
+
+public class ImageResponse {
+
+    private Long id;
+
+    private String imageUrl;
+
+    public ImageResponse() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
