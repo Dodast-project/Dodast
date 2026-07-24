41e52e5d969061cc6f38a9a37f5d6a98e5705579
Raman <raman.rezaei2007@gmail.com>
2026-07-24 10:37:51 +0330

    new : add AdminDashboardReponse to map incoming infos and store them to return w hen its required

---
+++ frontend/src/main/java/com/example/dodast/DTO/Admin/AdminDashboardResponse.java
@@ -0,0 +1,43 @@
+package com.example.dodast.DTO.Admin;
+
+public class AdminDashboardResponse {
+
+    private long userCount;
+    private long advertisementCount;
+    private long pendingAdvertisementCount;
+
+    public AdminDashboardResponse() {}
+
+    public AdminDashboardResponse(long userCount,
+            long advertisementCount,
+            long pendingAdvertisementCount) {
+                
+        this.userCount = userCount;
+        this.advertisementCount = advertisementCount;
+        this.pendingAdvertisementCount = pendingAdvertisementCount;
+    }
+
+    public long getUserCount() {
+        return userCount;
+    }
+
+    public long getAdvertisementCount() {
+        return advertisementCount;
+    }
+
+    public long getPendingAdvertisementCount() {
+        return pendingAdvertisementCount;
+    }
+
+    public void setUserCount(long userCount) {
+        this.userCount = userCount;
+    }
+
+    public void setAdvertisementCount(long advertisementCount) {
+        this.advertisementCount = advertisementCount;
+    }
+
+    public void setPendingAdvertisementCount(long pendingAdvertisementCount) {
+        this.pendingAdvertisementCount = pendingAdvertisementCount;
+    }
+}
